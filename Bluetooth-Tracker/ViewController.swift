ca62a0aa4e1d4207567fd02f8818a981facfe5d1
Koceto1973 <koceto1973@yahoo.ca>
2018-11-10 09:07:01 -0800

    code in

--- Bluetooth-Tracker/ViewController.swift
+++ Bluetooth-Tracker/ViewController.swift
@@ -7,31 +7,83 @@
 //
 
 import UIKit
+import CoreBluetooth
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, CBCentralManagerDelegate {
     
+    var centralManager : CBCentralManager?
+    var names : [String] = []
+    var RSSIs :[NSNumber] = []
+    var timer : Timer?
     
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        centralManager = CBCentralManager(delegate: self, queue: nil)
     }
     
-    @IBAction func refreshTapped(_ sender: Any) {
-        
+    @IBAction func refreshPressed(_ sender: Any) {
+        startScan()
+        startTimer()
     }
     
+    func startTimer() {
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(withTimeInterval: 10, repeats: true, block: { (timer) in
+            self.startScan()
+        })
+    }
+    
+    func startScan() {
+        names = []
+        RSSIs = []
+        tableView.reloadData()
+        centralManager?.stopScan()
+        centralManager?.scanForPeripherals(withServices: nil, options: nil)
+    }
+    
+    // CBCentralManger Code
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        
+        if let name = peripheral.name {
+            names.append(name)
+        } else {
+            names.append(peripheral.identifier.uuidString)
+        }
+        RSSIs.append(RSSI)
+        tableView.reloadData()
+    }
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if central.state == .poweredOn {
+            // Working
+            startScan()
+            startTimer()
+        } else {
+            // Not Working
+            let alertVC = UIAlertController(title: "Bluetooth isn't working", message: "Make sure your bluetooth is on and ready to rock and roll!", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "Ok", style: .default, handler: { (action) in
+                alertVC.dismiss(animated: true, completion: nil)
+            })
+            alertVC.addAction(okAction)
+            present(alertVC, animated: true, completion: nil)
+        }
+    }
     
     // TableView Code
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return names.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return cell
-    }
-    
-    
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "blueCell", for: indexPath) as? BluetoothTableViewCell {
+            cell.nameLabel.text = names[indexPath.row]
+            cell.rssiLabel.text = "RSSI: \(RSSIs[indexPath.row])"
+            return cell
+        }
+        return UITableViewCell()
+    }  
 }
